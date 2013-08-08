ebf4ec8788793a71eea4fb951a2617953be20704
thiago-oliveira <thiago.op93@gmail.com>
2013-08-08 10:51:17 -0300

    Ajustes

---
+++ learning/src/test/js/dogControllerSpec.js
@@ -0,0 +1,11 @@
+describe('PetShop controllers', function() {
+	describe('DogCtrl', function(){
+		it('should had no dogs list', function() {
+			var scope = {};
+			scope.dogs = null;
+			
+			expect(scope.dogs).toBe(null);
+			expect(scope.dogs).toBe(null);
+		});
+	});
+});
